051536b9cf4ff2c0875ae0171661dd7313b93860
Iryna Maslova <maslovai@hotmail.com>
2017-04-25 11:58:26 -0700

    lab partially done, needs debugging on put and update parts

--- starter-code/server.js
+++ starter-code/server.js
@@ -6,7 +6,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const conString = '';// TODO: Don't forget to set your own conString
+const conString = 'postgres://irynamaslova@localhost:5432/postgres';
+// TODO: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', function(error) {
@@ -24,7 +25,12 @@ app.get('/new', function(request, response) {
 app.get('/articles', function(request, response) {
   // REVIEW: This query will join the data together from our tables and send it back to the client.
   // TODO: Write a SQL query which joins all data from articles and authors tables on the author_id value of each
-  client.query(``)
+  client.query(`
+    SELECT articles.article_id, title, body, "publishedOn", authors.author AS author
+    FROM articles
+    INNER JOIN authors
+    ON articles.author_id = authors.author_id
+    ;`)
   .then(function(result) {
     response.send(result.rows);
   })
@@ -39,18 +45,29 @@ app.post('/articles', function(request, response) {
   //       Remember that client.query accepts two arguments: your SQL string and
   //       an array of values that it will replace in a 1-to-1 relationship
   //       with our placeholder values, signified with the syntax $1, $2, etc.
-  client.query(
-    '',
-    []
+  client.query(`
+    INSERT INTO authors(author, "authorUrl")
+    VALUES ($1, $2) ON CONFLICT DO NOTHING`,
+    [request.body.author,
+      request.body.authorUrl]
   )
   .then(function() {
     // TODO: Write a SQL query to insert a new article, using a sub-query to
     // retrieve the author_id from the authors table. HINT: How might we combine
     // the functionality of a SELECT with VALUES when inserting new rows?
     // TODO: Add the required values from the request as data for the SQL query to interpolate
-    client.query(
-      ``,
-      []
+    client.query(`
+      INSERT INTO
+      articles(author_id, title, category, "publishedOn", body)
+      SELECT author_id, $1, $2, $3, $4
+      FROM authors
+      WHERE author=$5;
+    `,
+      [request.body.author,
+      request.body.title,
+      request.body.category,
+      request.body.publishedOn,
+      request.body.body]
     )
   })
   .then(function() {
@@ -66,18 +83,28 @@ app.put('/articles/:id', function(request, response) {
   // an author_id property, so we can reference it from the request.body.
   // TODO: Add the required values from the request as data for the SQL query to interpolate
   client.query(
-    ``,
-    []
+    `UPDATE authors(author, "authorUrl")
+     VALUES ($1, $2)
+     WHERE author = $1`,
+    [request.body.author,
+      request.body.authorUrl]
   )
   .then(function() {
     // TODO: Write a SQL query to update an article record. Keep in mind that article records
     // now have an author_id, in addition to title, category, publishedOn, and body.
     // TODO: Add the required values from the request as data for the SQL query to interpolate
     client.query(
-      ``,
-      []
-    )
-  })
+      `UPDATE articles(author_id, title, category, "publishedOn", body)
+      SELECT author_id, $1, $2, $3, $4
+      FROM authors
+      WHERE author = $5;
+    `,
+      [request.body.title,
+       request.body.category,
+       request.body.publishedOn,
+       request.body.body,
+       request.body.author]
+  )
   .then(function() {
     response.send('Update complete');
   })
